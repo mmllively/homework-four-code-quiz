59d10d8021481c6331d192fc9803cb74162d09d1
Maggie Lively <maggielively@maggielvelyspro.lan>
2022-10-05 22:32:07 -0400

    adding first bit of code

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # homework-four-code-quiz
 Web API Code Quiz 
+
+## Description
+
+This quiz tests the ability to use JavaScript to rotate through questions, keep score and use a timer.
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+-...repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
+
+![alt text](assets/images/screenshot.png)
+
+## Credits
+
+All of the tutors, TAs and fellow UCF Bootcamp students.
+
---
+++ script.js
@@ -0,0 +1,203 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "What is the name of the smallest bone in human body?",
+    choices: ["Ulna", "Stapes", "Mandible", "Maxilla"],
+    answer: "Stapes",
+  },
+  {
+    title: "What is the largest muscle in the human body?",
+    choices: ["quadratus lumborum", "psoas", "gluteus maximus", "rectus femoris"],
+    answer: "gluteus maximus",
+  },
+  {
+    title: "How many bones are in the adult human body?",
+    choices: [
+      "206",
+      "300",
+      "187",
+      "126",
+    ],
+    answer: "206",
+  },
+  {
+    title:
+      "What is the largest organ in the body?",
+    choices: ["large intestine", "pancreas", "esophagus", "skin"],
+    answer: "skin",
+  },
+];
+
+// variables to keep track of quiz state
+// currentQuestion
+// time
+// timerId
+
+var currentQuestion = 0;
+var scoreTotal = 99;
+
+// variables to reference DOM elements
+var questionsEl = document.getElementById("questions");
+var startBtn = document.getElementById("start-btn");
+var feedBackElement = document.getElementById("feedback");
+
+/// FUNCTION TO START THE QUIZ
+function startQuiz() {
+  // console.log("start quiz");
+  // hide start screen
+  var startScreen = document.getElementById("start-screen");
+  startScreen.style.display = "none";
+  // un-hide questions section
+
+  var questionsToAsk = document.getElementById("questions");
+  questionsToAsk.style.display = "block";
+
+  // start timer
+
+  // show starting time
+
+  getQuestion(currentQuestion);
+}
+startBtn.addEventListener("click", startQuiz);
+
+/// FUNCTION FOR CLICKING A QUESTION ///
+function questionClick(event) {
+  console.log("question-click");
+  //this - coming from the function of "question-click", the "onclick" is calling the question-click, the btn is the button created that calls
+  console.log(this.value);
+  //value comes from the selection made (button clicked)
+ 
+
+  if (this.value !== questions[currentQuestion].answer) {
+    feedBackElement.textContent = "You're wrong! Try again!";
+    // window.alert("Wrong!");
+    // check if user guessed wrong
+    // penalize time
+    // display new time on page
+    // give them feedback, letting them know it's wrong
+  } else {
+    feedBackElement.textContent = "You're right!";
+    // window.alert("You're right!");
+    // give them feedback, letting them know it's right
+
+  }
+
+     // move to next question
+     // check if we've run out of questions
+  // if so, end the quiz
+  // else, get the next question
+     currentQuestion++
+     if (currentQuestion==questions.length){
+     quizEnd(); 
+     } else {
+      getQuestion(currentQuestion);
+     }
+
+  // flash right/wrong feedback on page for a short period of time
+  
+}
+/// FUNCTION TO GET/SHOW EACH QUESTION ///
+function getQuestion(currentQuestion) {
+  // get current question object from array
+
+  // console.log(questions[currentQuestion].title);
+
+  // update title with current question
+  document.getElementById("question-title").textContent =
+    questions[currentQuestion].title;
+
+  // clear out any old question choices
+  document.getElementById("choices").innerHTML = "";
+  // loop over choices
+  for (
+    let index = 0;
+    index < questions[currentQuestion].choices.length;
+    index++
+  ) {
+    // console.log(questions[currentQuestion].choices[index]);
+    // create new button for each choice
+    var btn = document.createElement("button");
+    // display on the page
+    btn.textContent = questions[currentQuestion].choices[index];
+    //give button a class, data attribute w/ value = text,
+    btn.setAttribute("class", "btnOption");
+    btn.setAttribute("value", questions[currentQuestion].choices[index]);
+    //add eventListener to click // note: appendChild will keep adding buttons unless question choices are cleared prior to the for loop
+    //onclick is an event that is defined as questionClick
+    btn.onclick = questionClick;
+    document.getElementById("choices").appendChild(btn);
+    
+  }
+
+  // get current answer
+  // check if correct answer
+  //set up click for other buttons
+}
+/// FUNCTION TO END THE QUIZ ///
+//show the end page 
+function quizEnd() {
+  console.log("end");
+ 
+  //look at local storage --> Must "setItem" before "getItem"
+  
+  // stop timer
+
+
+
+  // show end screen
+//why doesn't below work?
+//review "remove"
+  var endScreen = document.getElementById("end-screen");
+  endScreen.className = "hide"
+
+  function myFunction() {
+    var element = document.getElementById("myDIV");
+    element.classList.remove("mystyle");
+  }
+  // show final score
+  // hide questions section
+}
+/// CLICK EVENTS ///
+// user clicks button to submit initials
+
+// user clicks button to start quiz
+
+/// FUNCTION FOR UPDATING THE TIME ///
+function clockTick() {
+  // update time
+  // check if user ran out of time
+}
+
+function saveHighscore() {
+  // get value of input box - for initials
+  // make sure value wasn't empty
+  // get saved scores from localstorage, or if not any, set to empty array
+  // format new score object for current user
+  // save to localstorage
+  // redirect to next page
+}
+
+/// CLICK EVENTS ///
+// user clicks button to submit initials
+
+// user clicks button to start quiz
+
+// user clicks on element containing choices
+
+function pringHighscores() {
+  //either get scores from localstorage or set to an empty array
+  //sort highscores by score property in descending order
+  //loop through scores
+  //create li tag for each high score
+  //display on page
+}
+
+///FUNCTION TO CLEAR SCORES
+function clearHighscores() {
+  //remove an item from local storage
+  //reload the page
+}
+
+///CLICK EVENT TO RUN THE CLEAR SCORES FUNCTION
+// run function when page loads
+//   printHighscores();
